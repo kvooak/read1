fee38fe715a630251041f3c10a26c2111f9b5376
Quang Hoang <quang.hd@gmx.de>
2021-03-14 14:58:01 +0100

    WIP table row

--- frontend/src/components/CategoryTable.js
+++ frontend/src/components/CategoryTable.js
@@ -1,49 +1,106 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Table from 'react-bootstrap/Table';
 
 import { capitalizeFirstLetter } from '../widgets/utilities';
 
-export default function CategoryTable(category) {
-  const name = capitalizeFirstLetter(category.category);
+function CategorySnippetRow(props) {
+  const { snippet } = props;
+
+  const {
+    id,
+    content,
+    effect,
+    for_material_groups,
+    country_code,
+    timestamp,
+  } = snippet;
+
+  return (
+    <tr>
+      <td>{id.slice(-5)}</td>
+      <td>{content}</td>
+      <td>{effect}</td>
+      <td className="col">
+        {for_material_groups.map((mat) => (
+          <td className="row" key={mat}>{mat}</td>
+        ))}
+      </td>
+      <td>{country_code}</td>
+      <td>{timestamp}</td>
+    </tr>
+  );
+}
+
+CategorySnippetRow.propTypes = {
+  snippet: PropTypes.instanceOf(Object).isRequired,
+};
+
+export default function CategoryTable(props) {
+  const { category } = props;
+  const { name, data } = category;
+
+  const tableStyle = {
+    columnWidth: {
+      id: {
+        width: '2%',
+        maxWidth: '2%',
+        wordBreak: 'break-all',
+      },
+      content: {
+        width: '30%',
+        maxWidth: '30%',
+        wordBreak: 'break-all',
+      },
+      effect: {
+        width: '5%',
+        maxWidth: '5%',
+        wordBreak: 'break-all',
+      },
+      materialGroups: {
+        width: '20%',
+        maxWidth: '20%',
+        wordBreak: 'break-all',
+      },
+      countryCode: {
+        width: '5%',
+        maxWidth: '5%',
+        wordBreak: 'break-all',
+      },
+      dateModified: {
+        width: '10%',
+        maxWidth: '10%',
+        wordBreak: 'break-all',
+      },
+    },
+  };
 
   return (
     <div className="category-table">
-      <h4>{name}</h4>
-      <Table bordered size="sm">
+      <h4>{capitalizeFirstLetter(name)}</h4>
+      <Table bordered size="sm" responsive>
         <thead>
           <tr>
-            <th />
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
+            <th style={tableStyle.columnWidth.id}>ID</th>
+            <th style={tableStyle.columnWidth.content}>Content</th>
+            <th style={tableStyle.columnWidth.effect}>Effect</th>
+            <th style={tableStyle.columnWidth.materialGroups}>Material Groups</th>
+            <th style={tableStyle.columnWidth.countryCode}>Country Code</th>
+            <th style={tableStyle.columnWidth.dateModified}>Date Modified</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
+          {data.map((snippet) => <CategorySnippetRow key={snippet.id} snippet={snippet} />)}
         </tbody>
       </Table>
     </div>
   );
 }
 
-// CategoryTable.propTypes = {
-//   category: PropTypes.instanceOf(Object).isRequired,
-// };
+CategoryTable.propTypes = {
+  category: PropTypes.instanceOf(Object).isRequired,
+};
--- frontend/src/components/DataManagementScreen.js
+++ frontend/src/components/DataManagementScreen.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 
 import './DataManagementScreen.css';
@@ -13,14 +15,22 @@ export default function DataManagementScreen() {
   const [categories, setCategories] = useState([]);
 
   useEffect(() => {
-    setCategories(managementData.map((item) => {
+    let categoryNameList = managementData.map((item) => {
       if (categories.indexOf(item.category) === -1) {
         return item.category;
       }
       return null;
-    }));
+    });
 
-    setCategories((prev) => prev.filter((value, index, arr) => arr.indexOf(value) === index));
+    categoryNameList = categoryNameList.filter((value, index, arr) => arr.indexOf(value) === index);
+    const _categories = categoryNameList.map((category) => (
+      {
+        name: category,
+        data: managementData.filter((item) => item.category === category),
+      }
+    ));
+
+    setCategories(_categories);
   }, [managementData]);
 
   return (
@@ -28,7 +38,7 @@ export default function DataManagementScreen() {
       <h1>Data Management</h1>
       {categories?.map((category) => (
         <CategoryTable
-          key={category.category}
+          key={category.name}
           category={category}
         />
       ))}
