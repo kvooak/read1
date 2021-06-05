3ad7c8cc2c578979fcb85b2fc5960a18395fa5a3
Quang Hoang <quang.hd@gmx.de>
2021-06-05 15:04:57 +0200

    removed tailwindcss, installed emotion

--- frontend/src/components/DocumentScreen.js
+++ frontend/src/components/DocumentScreen.js
@@ -6,8 +6,11 @@ export default function DocumentScreen() {
   const [categories, setCategories] = useState([]);
 
   return (
-    <container>
-      <h1>Data Management</h1>
-    </container>
+    <div className="p-6 max-w-lg mx-auto bg-white rounded-lg border flex items-center space-x-12">
+      <div>
+        <div className="text-xl font-medium text-black">ChitChat</div>
+        <p className="text-gray-500">You have a new message!</p>
+      </div>
+    </div>
   );
 }
